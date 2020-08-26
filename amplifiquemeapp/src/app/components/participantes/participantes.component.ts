f6876d3db2c828afd54b0461293e08a6b5981a1a
kamila-almeida <almeidac.kamila@gmail.com>
2020-08-26 20:05:06 -0300

    * Criação do projeto em Angular 9 * Criação da estrutura do componente de participantes (sem estilo) * Implementação da leitura dos dados do arquivo data.json

---
+++ amplifiquemeapp/src/app/components/participantes/participantes.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import data from '../../../assets/data.json';
+
+@Component({
+  selector: 'app-participantes',
+  templateUrl: './participantes.component.html',
+  styleUrls: ['./participantes.component.scss']
+})
+export class ParticipantesComponent implements OnInit {
+
+  participantes = [];
+  constructor() { }
+
+  ngOnInit(): void {    
+    data.forEach(x => 
+      this.participantes.push(x)  
+    )
+    console.log(this.participantes);
+  }
+
+}
